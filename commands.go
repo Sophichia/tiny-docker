c2b34f5d13a33080275d4c60113bcfbf35723ff7
Mengjia Liang <liang_mengjia@126.com>
2020-01-07 16:34:22 -0800

    Add the flags for run command

--- commands.go
+++ commands.go
@@ -1,23 +1,53 @@
 package main
 
-import "github.com/urfave/cli"
+import (
+	"github.com/urfave/cli"
+)
 
 var runCommand = cli.Command{
-	Name:                   "run",
-	Usage:                  `Create a container with namespace and cgroup limit.`,
-	UsageText:              "Example: tiny-docker run -ti [image] [command]",
+	Name:      "run",
+	Usage:     `Create a container with namespace and cgroup limit.`,
+	UsageText: "Example: tiny-docker run -ti [image] [command]",
 	Flags: []cli.Flag{
 		&cli.BoolFlag{
-			Name: "ti",
+			Name:  "ti",
 			Usage: "enable tty",
 		},
 		&cli.BoolFlag{
-			Name: "d",
+			Name:  "d",
 			Usage: "detach container",
 		},
 		&cli.StringFlag{
-			Name: "m",
+			Name:  "m",
 			Usage: "memory limit",
+		},
+		&cli.StringFlag{
+			Name:  "cpushare",
+			Usage: "cpushare limit",
+		},
+		&cli.StringFlag{
+			Name:  "cpuset",
+			Usage: "cpuset limit",
+		},
+		&cli.StringFlag{
+			Name:  "name",
+			Usage: "container name",
+		},
+		&cli.StringFlag{
+			Name:  "v",
+			Usage: "volume",
+		},
+		&cli.StringFlag{
+			Name:  "e",
+			Usage: "set environment variables",
+		},
+		&cli.StringFlag{
+			Name:  "net",
+			Usage: "container network",
+		},
+		&cli.StringFlag{
+			Name:  "p",
+			Usage: "port mapping",
 		},
 	},
 }
