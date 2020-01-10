48e94124125eba9c786105f583c034e530649721
Mengjia Liang <liang_mengjia@126.com>
2020-01-10 14:51:30 -0800

    Add initCommand and runCommand

--- main.go
+++ main.go
@@ -1,51 +1,20 @@
 package main
 
 import (
-	"fmt"
 	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 	"os"
 )
 
-var app = cli.NewApp()
-
-var pizza = []string{"Enjoy you test"}
-
-func info() {
-	app.Name = "Simple CLI"
-	app.Usage = "An example CLI"
-	app.Version = "1.0.0"
-}
-
-func commands() {
-	app.Commands = []*cli.Command{
-		{
-			Name: "create",
-			Aliases: []string{"c"},
-			Usage: "Test create",
-			Action: func(c *cli.Context) error {
-				hello := "hello"
-				fmt.Println(hello)
-				return nil
-			},
-		},
-		{
-			Name: "delete",
-			Aliases: []string{"d"},
-			Usage: "Test delete",
-			Action: func(c *cli.Context) error {
-				hello := "world"
-				fmt.Println(hello)
-				return nil
-			},
-		},
-	}
-}
-
 func main() {
 	app := cli.NewApp()
 	setInformation(app)
 	setCommand(app)
+
+	app.Commands = []*cli.Command{
+		&initCommand,
+		&runCommand,
+	}
 
 	app.Before = func(context *cli.Context) error {
 		// Log as JSON format instead of ASCII formatter
