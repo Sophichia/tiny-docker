5a6d09c8aae6b24493c27fcd17a20f33b25ef15a
Mengjia Liang <liang_mengjia@126.com>
2020-01-10 12:19:01 -0800

    Modify commands version to chapter 3

--- commands.go
+++ commands.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"github.com/Sophichia/tiny-docker/container"
+	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 )
 
@@ -49,5 +52,62 @@ var runCommand = cli.Command{
 			Name:  "p",
 			Usage: "port mapping",
 		},
+	},
+	Action: func(c *cli.Context) error {
+		if c.Args().Len() < 1 {
+			return fmt.Errorf("Missing container command! ")
+		}
+		cmd := c.Args().Get(0)
+		tty := c.Bool("ti")
+		Run(tty, cmd)
+		return nil
+		//
+		//var cmdArray []string
+		//for _, cmd := range c.Args().Slice() {
+		//	cmdArray = append(cmdArray, cmd)
+		//}
+		//
+		//imageName := cmdArray[0]
+		//cmdArray = cmdArray[1:]
+		//
+		//createTty := c.Bool("ti")
+		//detach := c.Bool("d")
+		//
+		//if createTty && detach {
+		//	return fmt.Errorf("ti and d param cannot set together! ")
+		//}
+		//
+		//resConfig := &subsystems.ResourceConfig{
+		//	MemoryLimit: c.String("m"),
+		//	CpuSet: c.String("cpuset"),
+		//	CpuShare: c.String("cpushare"),
+		//}
+		//
+		//log.Infof("create tty: %v", createTty)
+		//containerName :=  c.String("name")
+		//volume := c.String("v")
+		//network := c.String("net")
+		//
+		//envSlice := c.StringSlice("e")
+		//portMapping := c.StringSlice("p")
+		//
+		//err := Run(createTty, cmdArray, resConfig, containerName, volume, imageName, envSlice, network, portMapping)
+		//if err != nil {
+		//	return fmt.Errorf("run container command fails %v", err)
+		//}
+		//
+		//return nil
+	},
+}
+
+var initCommand = cli.Command{
+	Name: "init",
+	Usage: "Init container process run user's proocess in container. Do not call it outside",
+	Action: func(c *cli.Context) error {
+		log.Infof("start init process")
+		cmd := c.Args().Get(0)
+		log.Info("command %s", cmd)
+		err := container.RunContainerInitProcess(cmd, nil)
+		return err
 	},
 }
